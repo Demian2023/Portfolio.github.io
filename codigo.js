f3b39c4c098c1e4a0101047ea6fd1f171f8fa62b
Demian2023 <116525747+Demian2023@users.noreply.github.com>
2023-03-03 00:13:57 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,35 +2,54 @@ const contacto = () => {
     window.open("https://api.whatsapp.com/send?phone=5493462662288&text=");
 }
 
-// document.addEventListener("mousemove", parallax)
+"use strict";
 
-// function parallax(e) {
-//     document.querySelectorAll(".objeto").forEach(function(move) {
-//         var x = e.clientX / 15;
-//         var y = e.clientY / 15;
+const trigger = document.querySelectorAll(".trigger");
 
-//         move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
-//     });
-// }
-
-var tl = gsap.timeline();
-tl.from(".foto", { delay: 1, duration: 2, opacity: 0, x: 200, rotate: 25, ease: "power2" });
-tl.from(".soy", { duration: 1.5, y: 100, x: '-100vw', ease: "power1" }, "-=1.5");
+const cuerpoDer = document.getElementsByClassName("cuerpoDer")[0];
+const soy = document.getElementsByClassName("soy")[0];
 
 
-const cuerpoRedes = document.querySelector('.cuerpoRedes');
+const verifyVisibility = (entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            cuerpoDer.classList.add('mov');
+            soy.classList.add('movSoy');
+        } else {
+            cuerpoDer.classList.remove('mov');
+            soy.classList.remove('movSoy');
+        }
+    }
+}
 
-gsap.registerPlugin(ScrollTrigger);
+const observer = new IntersectionObserver(verifyVisibility);
 
-gsap.from(".cuerpoRedes", {
-    scrollTrigger: {
-        trigger: ".cuerpoRedes",
-        toggleActions: "restart restart",
-    },
-    delay: .25,
-    x: "-100vw",
-    duration: 2,
-})
+for (const caja of trigger) {
+    observer.observe(caja);
+}
+
+// segundo observer
+
+const triggerCuerpoRedes = document.querySelectorAll(".sobreMiMov");
+
+const sobreMiMov = document.getElementsByClassName("sobreMiMov")[0];
+
+
+const verifyVisibility2 = (entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            sobreMiMov.classList.add('sobreMi');
+        } else {
+            sobreMiMov.classList.remove('sobreMi');
+        }
+    }
+}
+
+const observer2 = new IntersectionObserver(verifyVisibility2);
+
+for (const caja of triggerCuerpoRedes) {
+    observer2.observe(caja);
+}
 
 const boton = document.getElementsByClassName("toggle")[0]
 const navBoton = document.getElementsByClassName("der")[0]
